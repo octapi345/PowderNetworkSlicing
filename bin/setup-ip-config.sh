2be188b3563bba4cf126e94a64b2c78035de9f80
David M. Johnson <johnsond@flux.utah.edu>
2022-04-01 11:02:30 -0600

    Add a route to the O-RAN/k8s service subnet.

--- bin/setup-ip-config.sh
+++ bin/setup-ip-config.sh
@@ -7,3 +7,4 @@ SUDO=/usr/bin/sudo
 $SUDO /sbin/iptables -t nat -A POSTROUTING -o `cat /var/emulab/boot/controlif` -j MASQUERADE
 $SUDO /sbin/sysctl -w net.ipv4.ip_forward=1
 $SUDO /sbin/ip route add 10.233/16 via $GW
+$SUDO /sbin/ip route add 10.96/12 via $GW
