df6b6e2c902eb5a838b5a16c20645b8b3cd9d2e6
octapi345 <118294938+octapi345@users.noreply.github.com>
2023-03-29 15:58:38 -0400

    Update email-server.sh

--- email-server.sh
+++ email-server.sh
@@ -1,8 +1,14 @@
 sudo apt-get update
 sudo apt-get upgrade
+
+#postfix setup
 sudo debconf-set-selections <<< "postfix postfix/mailname string emailserver.emulab.net"
 sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 sudo apt-get install --assume-yes postfix
+
+#program for viewing network statistics
+sudo apt-get install vnstat
+
 #uneeded
 #sudo apt-get install dovecot-pop3d
 
