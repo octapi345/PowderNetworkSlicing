25482b8044d47eb3d58e2b6cd21635efc845f677
octapi345 <118294938+octapi345@users.noreply.github.com>
2023-03-29 15:40:39 -0400

    automate postfix setup

--- email-server.sh
+++ email-server.sh
@@ -1,6 +1,8 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install postfix
+sudo debconf-set-selections <<< "postfix postfix/mailname string emailserver.emulab.net"
+sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+sudo apt-get install --assume-yes postfix
 #uneeded
 #sudo apt-get install dovecot-pop3d
 
