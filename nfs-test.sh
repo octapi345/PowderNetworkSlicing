7638a5a70aae6ce0d6ff4dbdb377693df9abb0c8
octapi345 <118294938+octapi345@users.noreply.github.com>
2023-04-06 18:13:39 -0400

    Create nfs-test.sh
    nfs testing scenario

---
+++ nfs-test.sh
@@ -0,0 +1,14 @@
+trap "exit;" SIGINT
+cd /nfs
+i=1
+text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea re"
+while true
+do
+  touch $1-$i.txt
+  echo "$text" >> $1-$i.txt
+  sleep 0.5
+  cp $1-$i.txt /copy
+  sleep 0.5
+  let i++
+done
+  
