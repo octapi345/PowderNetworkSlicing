ac4225dfeac4985a8caf01523a02724884da8cc8
octapi345 <118294938+octapi345@users.noreply.github.com>
2023-04-07 12:02:03 -0400

    Update dns-test.sh

--- dns-test.sh
+++ dns-test.sh
@@ -1,5 +1,4 @@
 trap "exit;" SIGINT
-i=1
 for a in {a..z}
 do
   for b in {a..z}
@@ -7,7 +6,7 @@ do
     for c in {a..z}
     do
       echo "DNS lookup test"
-      dig emailserver $a$b.%c.com
+      dig @emailserver $a$b.$c.com
       sleep 0.5
     done
   done    
