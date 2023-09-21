d26ece41e0c80fe2e525ce2b322ff847a9c4dcca
Adam Seliga <46682225+adameseliga@users.noreply.github.com>
2023-09-21 19:17:26 -0400

    Update email-test.sh

--- email-test.sh
+++ email-test.sh
@@ -6,7 +6,7 @@ fi
 while true
 do
   echo "sending  email $i"
-  (time (mail -s "test $i" $1 < /local/repository/email.txt)) /local/repository/send.txt >> 2>1&
+  (time (mail -s "test $i" $1 < /local/repository/email.txt)) /local/repository/send.txt >> 2>&1
   let i++;
   sleep 0.05
 done
