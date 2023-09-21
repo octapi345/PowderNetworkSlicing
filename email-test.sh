cfe8ae7acf2bd35e81a34228f1cefe6bc7a91a09
Adam Seliga <46682225+adameseliga@users.noreply.github.com>
2023-09-21 19:17:12 -0400

    Update email-test.sh with logging output to send.txt

--- email-test.sh
+++ email-test.sh
@@ -1,9 +1,12 @@
 trap "exit;" SIGINT
 i=1
+if [! -e /local/repository/send.txt]; then
+  touch /local/repository/send.txt
+fi
 while true
 do
   echo "sending  email $i"
-  mail -s "test $i" $1 < /local/repository/email.txt
+  (time (mail -s "test $i" $1 < /local/repository/email.txt)) /local/repository/send.txt >> 2>1&
   let i++;
   sleep 0.05
 done
