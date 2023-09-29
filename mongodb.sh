2075402b964a6656326ec8ab090adb7b4b051959
Adam Seliga <46682225+adameseliga@users.noreply.github.com>
2023-09-28 20:37:22 -0400

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,7 @@ curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
    --dearmor
 
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-sudo apt-get update && sudo apt-get install -y mongodb-org
+sudo apt-get update && sudo apt-get install -y mongodb-org-database mongodb-org
 
 sudo systemctl enable mongod
 sudo systemctl start mongod
