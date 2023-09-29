f8002849272680778a9523283f321420b5772e20
Adam Seliga <46682225+adameseliga@users.noreply.github.com>
2023-09-28 20:31:23 -0400

    Create mongodb.sh
    Install and run MongoDB on localhost

---
+++ mongodb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update && sudo apt-get install -y mongodb-org
+
+sudo systemctl enable mongod
+sudo systemctl start mongod
+
