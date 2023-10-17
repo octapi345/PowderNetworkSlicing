2187215819326073bb463f379cad47903bb73be0
octapi345 <118294938+octapi345@users.noreply.github.com>
2023-10-17 06:43:50 -0400

    Update filter.py
    added a count of number of transactions to get transaction per second metric

--- filter.py
+++ filter.py
@@ -6,11 +6,14 @@ f.close()
 edit = re.findall("m.*s", txt)
 times = []
 sum=0
+transactNum=0
 for x in range(len(edit)):
         times.append(float(edit[x].strip("ms")))
 
 for i in range(len(times)):
+        transactNum++;
         sum+=times[i]
 
 avg=sum/len(times)
 print(avg)
+print(transactNum)
