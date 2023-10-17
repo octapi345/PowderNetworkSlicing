48f6d98a3095b036ae547c5509d0d3d47f53baad
octapi345 <118294938+octapi345@users.noreply.github.com>
2023-10-17 06:50:25 -0400

    Update filter.py
    python doesnt allow ++

--- filter.py
+++ filter.py
@@ -11,7 +11,7 @@ for x in range(len(edit)):
         times.append(float(edit[x].strip("ms")))
 
 for i in range(len(times)):
-        transactNum++
+        transactNum+=1
         sum+=times[i]
 
 avg=sum/len(times)
