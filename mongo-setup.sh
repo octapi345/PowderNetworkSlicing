52300aef42db133e9772e626a6d458ac83d97ca1
octapi345 <118294938+octapi345@users.noreply.github.com>
2023-11-10 14:00:11 -0500

    Update mongo-setup.sh

--- mongo-setup.sh
+++ mongo-setup.sh
@@ -9,7 +9,7 @@ curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get install -y mongodb
 
-cp -v /local/repository/dbconfig.txt /etc/mongodb.config
+sudo cp -v /local/repository/dbconfig.txt /etc/mongodb.config
 
 ### Mongosh Install
 wget -qO- https://www.mongodb.org/static/pgp/server-7.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-7.0.asc
