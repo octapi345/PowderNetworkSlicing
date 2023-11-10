04bdc32690bc7654415f445a042361aa048eb110
octapi345 <118294938+octapi345@users.noreply.github.com>
2023-11-10 04:48:30 -0500

    Update mongo_measure.sh
    now gets response time from python file output

--- TestScripts/mongo_measure.sh
+++ TestScripts/mongo_measure.sh
@@ -3,10 +3,10 @@ trap "exit;" SIGINT
 
 # Path to your Python script
 
-PYTHON_SCRIPT_PATH="/local/repository/TestScripts/mongo_test.py nfs.aseliga-173674.networkslicing.emulab.net"
+PYTHON_SCRIPT_PATH="/local/repository/TestScripts/mongo_test.py input" #add mongodb server hostname instead of input
 
 # Number of iterations
-N_ITERATIONS=10
+N_ITERATIONS=1
 
 # Output file for TPS results
 OUTPUT_FILE="/local/repository/TestScripts/tps_results.txt"
@@ -17,22 +17,22 @@ OUTPUT_FILE="/local/repository/TestScripts/tps_results.txt"
 # Main loop
 for i in $(seq 1 $N_ITERATIONS); do
     # Capture the start time
-    START_TIME=$(date +%s.%N)
+    #START_TIME=$(date +%s.%N)
     
     # Execute the Python script
-    python3 $PYTHON_SCRIPT_PATH
+    python3 $PYTHON_SCRIPT_PATH >> $OUTPUT_FILE
     
     # Capture the end time
-    END_TIME=$(date +%s.%N)
+    #END_TIME=$(date +%s.%N)
     
     # Calculate the duration
-    DURATION=$(echo "$END_TIME - $START_TIME" | bc)
+    #DURATION=$(echo "$END_TIME - $START_TIME" | bc)
     
     # Calculate transactions per second (assuming one transaction per run for simplicity)
-    TPS=$(echo "1 / $DURATION" | bc -l)
+    #TPS=$(echo "1 / $DURATION" | bc -l)
     
     # Write the TPS result to the output file
-    echo "Iteration $i: $TPS transactions/second" >> $OUTPUT_FILE
+    #echo "Iteration $i: $TPS transactions/second" >> $OUTPUT_FILE
 done
 
 echo "TPS results written to $OUTPUT_FILE"
