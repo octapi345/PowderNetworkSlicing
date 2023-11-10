c25af0f2c44239358c8255731e325319d256f4e3
octapi345 <118294938+octapi345@users.noreply.github.com>
2023-11-10 04:45:19 -0500

    Update mongo_test.py
    changed loop to happen inside python file so metrics weren't influenced by import statements being run repeatedly

--- TestScripts/mongo_test.py
+++ TestScripts/mongo_test.py
@@ -1,27 +1,28 @@
-from pymongo import MongoClient
-import csv
-import sys
+for i in range(100000):
+    start=time.time()
+    mongo_host = sys.argv[1]
+    client = MongoClient(f"mongodb://{mongo_host}:27017")
+    db = client['testdb']
+    collection = db['testcollec']
 
-mongo_host = sys.argv[1]
-client = MongoClient(f"mongodb://{mongo_host}:27017")
+    with open("/local/repository/testingdata.csv", "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        sum=0
+        for row in csv_reader:
+            # Convert the CSV row to a dictionary
+            data = {
+                "name": row["name"],
+                "phone": row['phone'],
+                "email": row["email"],
+                "address": row['address'],
+                "postalZip": row['postalZip'],
+                "list": int(row['list']),
+                "country": row['country']
+            }
 
-db = client['testdb']
-collection = db['testcollec']
-
-with open("/local/repository/testdata.csv", "r") as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    
-    for row in csv_reader:
-        # Convert the CSV row to a dictionary
-        data = {
-            "name": row["name"],
-            "phone": row['phone'],
-            "email": row["email"],
-            "address": row['address'],
-            "postalZip": row['postalZip'],
-            "list": int(row['list']),
-            "country": row['country']
-        }
-
-        # Insert the document into the collection
-        collection.insert_one(data)
+            # Insert the document into the collection
+            collection.insert_one(data)
+        end = time.time()
+        sum=1000*(end-start)
+        print(f"{sum}ms")
+        time.sleep(0.5)
