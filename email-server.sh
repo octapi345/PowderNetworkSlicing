d7cb8d809fc386b2c27fb58aa40f40c5e2b7335c
octapi345 <118294938+octapi345@users.noreply.github.com>
2023-11-10 00:24:08 -0500

    Update email-server.sh
    prevent emails from being sent twice if first email fails (messes with data)

--- email-server.sh
+++ email-server.sh
@@ -6,6 +6,9 @@ sudo debconf-set-selections <<< "postfix postfix/mailname string emailserver.emu
 sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 sudo apt-get install --assume-yes postfix
 
+#This prevents postfix from resending emails on fail
+sudo postconf maximal_queue_lifetime=0
+
 #program for viewing network statistics
 sudo apt-get install vnstat
 
